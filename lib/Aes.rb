00db8ef25d8e2b76431df3446e57dcdb763a0e45
Austin Hester <ahester@incomm.com>
2020-07-19 23:12:20 -0500

    AES encrypt / decrypt
    Signed-off-by: Austin Hester <ahester@incomm.com>

---
+++ lib/Aes.rb
@@ -0,0 +1,34 @@
+# Aes.rb
+# ruby 2.6.6p146 (2020-03-31 revision 67876) [x64-mingw32]
+
+require 'openssl'
+
+class Aes
+    def initialize
+        @key = nil
+        @iv = nil
+    end
+
+    def encrypt(plaintext)
+        cipher = OpenSSL::Cipher::AES.new(128, :CBC)
+        cipher.encrypt
+        @key = cipher.random_key
+        @iv = cipher.random_iv
+        #https://docs.ruby-lang.org/en/2.4.0/OpenSSL/Cipher.html#class-OpenSSL::Cipher-label-Calling+Cipher-23final
+        encrypted = cipher.update(plaintext) + cipher.final
+
+    end
+
+    def decrypt(ciphertext)
+        if (@key == nil) then
+            p 'no do decrypt first'
+            return
+        end
+        decipher = OpenSSL::Cipher::AES.new(128, :CBC)
+        decipher.decrypt
+        decipher.key = @key
+        decipher.iv = @iv
+        decrypted = decipher.update(ciphertext) + decipher.final
+    end
+
+end
--- lib/Hester_Project2.rb
+++ lib/Hester_Project2.rb
@@ -1,6 +1,7 @@
 # Hester_Project2.rb
 # ruby 2.6.6p146 (2020-03-31 revision 67876) [x64-mingw32]
 
+require_relative 'Aes'
 
 class Hester_Project2
 	def initialize
@@ -10,17 +11,14 @@ class Hester_Project2
 
     private def menu
         puts( "\n1:\tAES Encrypt" )
-        puts( "2:\tAES Decrypt" )
-        puts( "3:\tHash" )
+        puts( "2:\tHash" )
         puts( "0:\tExit" )
         response = readline
         case response.chars[0]
         when "1"
-            menu_encrypt
+            aes_encrypt_and_decrypt
         when "2"
-            menu_decrypt
-        when "3"
-            menu_test
+            Aes.new.test
         when "0"
             puts "Goodbye."
         else
@@ -28,13 +26,16 @@ class Hester_Project2
         end
     end
 
-    private def menu_encrypt
-        puts 'encryp'
-        menu
-    end
-
-    private def menu_decrypt
-        puts 'dencryp'
+    private def aes_encrypt_and_decrypt
+        aes = Aes.new
+        print( "\nEnter plain text: " )
+        plain_text = readline
+        puts 'encrypting'
+        cipher = aes.encrypt(plain_text)
+        puts( "Cipher:\t" + cipher )
+        puts 'decrypting'
+        plain_again = aes.decrypt(cipher)
+        puts( "Plain:\t" + plain_again )
         menu
     end
 
