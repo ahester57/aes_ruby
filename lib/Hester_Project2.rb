9d9c5ab094593b9afa47aa9970174e25f2e7c79f
Austin Hester <ahester57@gmail.com>
2020-07-21 00:40:03 -0400

    encrypt/decrypt bullcrap
    Signed-off-by: Austin Hester <ahester57@gmail.com>

--- lib/Hester_Project2.rb
+++ lib/Hester_Project2.rb
@@ -7,20 +7,25 @@ require_relative 'Lorem'
 class Hester_Project2
 	def initialize
         $stdout.sync = true
+        @lorem = Lorem.new
         menu
 	end
 
     private def menu
         puts( "\n1:\tAES Encrypt" )
         puts( "2:\tGenerate Bullcrap" )
+        puts( "3:\tEncrypt/Decrypt Files" )
         puts( "0:\tExit" )
         response = readline
         case response.chars[0]
         when "1"
-            aes_encrypt_and_decrypt
+            aes_encrypt_and_decrypt(Aes.new)
         when "2"
-            lorem = Lorem.new
-            lorem.make_files
+            @lorem.make_files
+            menu    # i know recursion is bad for menus. T-23 min til due
+            return
+        when "3"
+            encrypt_decrypt_files(Aes.new)
         when "0"
             puts "Goodbye."
         else
@@ -28,22 +33,52 @@ class Hester_Project2
         end
     end
 
-    private def aes_encrypt_and_decrypt
-        aes = Aes.new
+    private def aes_encrypt_and_decrypt(aes)
         print( "\nEnter plain text: " )
         plain_text = readline
-        puts 'encrypting'
-        cipher = aes.encrypt(plain_text)
+        cipher = encrypt(aes, plain_text)
         puts( "Cipher:\t" + cipher )
-        puts 'decrypting'
-        plain_again = aes.decrypt(cipher)
+        plain_again = decrypt(aes, cipher)
         puts( "Plain:\t" + plain_again )
         menu
     end
 
-    private def menu_hash
-        puts 'hahshs'
-        menu
+    private def encrypt(aes, msg)
+        puts 'encrypting'
+        aes.encrypt(msg)
+    end
+
+    private def decrypt(aes, msg)
+        puts 'decrypting'
+        aes.decrypt(msg)
+    end
+
+    private def encrypt_decrypt_files(aes)
+        puts( "\nHow Big" )
+        puts( "1:\ttiny" )
+        puts( "2:\tmedium" )
+        puts( "3:\tvv big" )
+        puts( "0:\tExit" )
+        response = readline
+        case response.chars[0]
+        when "1"
+            plain_text = @lorem.get_small
+        when "2"
+            plain_text = @lorem.get_medium
+        when "3"
+            plain_text = @lorem.get_big
+        when "0"
+            menu
+            return
+        else
+            encrypt_decrypt_files(aes)
+            return
+        end
+        cipher = encrypt(aes, plain_text)
+        puts( "Cipher:\t" + cipher )
+        plain_again = decrypt(aes, cipher)
+        puts( "Plain:\t" + plain_again )
+        encrypt_decrypt_files(aes)
     end
 
 end
--- lib/Lorem.rb
+++ lib/Lorem.rb
@@ -1,9 +1,14 @@
-# Hester_Project2.rb
+# Lorem.rb
 # ruby 2.6.6p146 (2020-03-31 revision 67876) [x64-mingw32]
 
 require 'betterlorem'
 
 class Lorem
+    def initialize
+        @small = "./out/bs_small.txt"
+        @medium = "./out/bs_medium.txt"
+        @big = "./out/bs_big.txt"
+    end
 
     def make_files
         if (!Dir.exists?("./out")) then
@@ -11,11 +16,11 @@ class Lorem
         end
 
         f1 = generate_bullcrap(3)
-        File.write("./out/bs_small.txt", f1)
+        File.write(@small, f1)
         f2 = generate_bullcrap(3000)
-        File.write("./out/bs_medium.txt", f2)
-        f3 = generate_bullcrap(3000) * 100
-        File.write("./out/bs_big.txt", f3)
+        File.write(@medium, f2)
+        f3 = generate_bullcrap(3000) * 400
+        File.write(@big, f3)
 
         puts( "Bullcrap written to ./out" )
     end
@@ -23,4 +28,17 @@ class Lorem
     def generate_bullcrap(how_much)
         BetterLorem.c(how_much, true)  # <- Generate characters
     end
+
+    def get_small
+        File.read(@small)
+    end
+
+    def get_medium
+        File.read(@medium)
+    end
+
+    def get_big
+        File.read(@big)
+    end
+
 end
